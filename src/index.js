e6edec559b4924950044535c87a3dfc13008ff8a
Ammar S <60483997+asiddiqi18@users.noreply.github.com>
2022-01-17 13:29:22 -0600

    added font-awesome

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./components/App.js";
 import registerServiceWorker from "./registerServiceWorker";
 import 'bootstrap/dist/css/bootstrap.css'
+import "font-awesome/css/font-awesome.css";
 
 ReactDOM.render(
   <App />,
